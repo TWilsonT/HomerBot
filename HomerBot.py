13fb6f86c91f72ba86d1e21082c8645645d492ce
Travis <1traviswilson@gmail.com>
2017-06-30 19:21:31 -0700

    Created the HomerBot.py file

---
+++ HomerBot.py
@@ -0,0 +1,12 @@
+import twitter
+
+api = twitter.Api(consumer_key='',
+                      consumer_secret='',
+                      access_token_key='-',
+                      access_token_secret='')
+
+print(api.VerifyCredentials())
+print "fart"
+
+status = api.PostUpdate('Hello, I am the Blue Jays Home Run Bot!')
+print status
