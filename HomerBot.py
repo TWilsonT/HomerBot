b5b6b00a0bd2ead0864b2062763845842a31ad3f
Travis <1traviswilson@gmail.com>
2017-07-05 20:04:00 -0700

    Can now parse the data from a web page

--- HomerBot.py
+++ HomerBot.py
@@ -1,37 +1,67 @@
+import sys
+
 import twitter
 import urllib2
 import re
 
-HOMRUN_GROUP = 2
+## Constants and Global Variables
+TEAM_SHORT_NAME = "TOR"
+HOMERUN_GROUP = 2
+PLAYER_GROUP = 0
+homerunData = []
+
+## temporary until I can extract this from the page
 boxScoreURL = "http://www.cbssports.com/mlb/gametracker/boxscore/MLB_20170705_TOR@NYY/"
-api = twitter.Api(consumer_key='skESbPn16O3VfJQFyH33DnrYI',
-                      consumer_secret='a3Ke5lhjSA28TmQ3bLaeZGUnyM74EB6kYGdrU7VdTlcZKQiAcY',
-                      access_token_key='880951260312068096-oQ5FU0dtyvpGJC4aS2e8TRbDrf0QpRn',
-                      access_token_secret='hVmTdkT4aZu4oetLOwE4xwhH06VsTTOaKjeUnvUS2Racz')
 
-#print(api.VerifyCredentials())
+api = twitter.Api(consumer_key='',
+                      consumer_secret='',
+                      access_token_key='-',
+                      access_token_secret='')
 
+try:
+	print api.VerifyCredentials()
+except:
+	print "Could Not Connect to Twitter, Exiting Program"
+	sys.exit()
 
+def postHomeRunToTwitter(playerData):
+	playerDataRegex = re.compile('^(\w+\. \w*) \((\d+)\)*$')
 
-#status = api.PostUpdate('Hello, I am the Blue Jays Home Run Bot!')
-#print status
+	result = re.match(playerDataRegex, playerData)
+	result = result.groups()
+
+	playerName = result[0]
+	playerHomerCount = result[1]
+
+	twitterMessage = "Home Run for " + playerName + " his " + playerHomerCount + "th of the season!"
+	print twitterMessage
+	#status = api.PostUpdate(twitterMessage)
+
 
 rawHTML = urllib2.urlopen(boxScoreURL).read()
 
-## get the home run data for both teams
+## get the home run data for both teams (want to pair this expression down)
 homerunRegex = re.compile("((.|\n)*)HR: </span\><!-- react-text: 121 -->((.|\n)*)<!-- /react-text --></span></li>(.|\n)*")
 
 result = re.match(homerunRegex, rawHTML)
 result = result.groups()
 
-result = result[2]
-## narrow down to just toronto
+homeRunText = result[HOMERUN_GROUP]
 
-torontoRegex = re.compile("TOR -(.*), \w\w\w -.*")
-result = re.match(torontoRegex, result)
+## narrow down to just the team we care about
+# create the regular expression
+teamNameRegex = TEAM_SHORT_NAME + " -(.*), \w\w\w -.*"
+teamNameRegex = re.compile(teamNameRegex)
 
+# extract the home run info
+result = re.match(teamNameRegex, homeRunText)
 result = result.groups()
-result = result[0]
+playerText = result[PLAYER_GROUP]
 
-result = result.split(",")
-print result
+# get the individual player data
+playerData = playerText.split(",")
+for player in range(0, len(playerData)):
+	playerData[player] = playerData[player].strip()
+	if playerData[player] not in homerunData:
+		postHomeRunToTwitter(playerData[player])
+		homerunData.append(playerData[player])
